e40cc2fd68af77548953a9867bb9680ef8497397
Shivang Bhandari <pump382156@gmail.com>
2017-07-12 14:24:00 +0530

    Basic script added

---
+++ paisa.py
@@ -0,0 +1,22 @@
+#!/Users/grim/anaconda/bin/python3
+
+import requests
+
+print("Enter the amount to Convert from USD to INR : ")
+
+toConvert = input()
+
+response = requests.get(url="http://www.apilayer.net/api/live?access_key=8afdbe8d25317768468dbe6fe5725fd7&format=1")
+
+jsonResponse = response.json()
+
+priceList= jsonResponse["quotes"]
+
+usdinr = priceList["USDINR"]
+
+print("The current USD Price is : Rs."+str(usdinr))
+
+print(float(toConvert)*usdinr)
+
+
+
