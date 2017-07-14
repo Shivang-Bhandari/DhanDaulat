cecf9088cda91f688dd59e9c3491ca916f3371c0
Shivang Bhandari <pump382156@gmail.com>
2017-07-15 01:09:28 +0530

    Comments added

--- dhandaulat/paisa.py
+++ dhandaulat/paisa.py
@@ -5,16 +5,23 @@ import requests
 def main():
     print("Enter the amount to Convert from USD to INR : ")
 
+    # takes user input for amount ot convert
     toConvert = input()
 
+    # fetches current price for USD
     response = requests.get(url="http://www.apilayer.net/api/live?access_key=8afdbe8d25317768468dbe6fe5725fd7&format=1")
 
+    # converts the response
     jsonResponse = response.json()
 
+    # fetches list of currency
     priceList = jsonResponse["quotes"]
 
+    # fetches USD to INR Conversion rate
     usdinr = priceList["USDINR"]
 
+
+    # Output
     print("The current USD Price is : Rs." + str(usdinr))
 
     print(float(toConvert) * usdinr)
